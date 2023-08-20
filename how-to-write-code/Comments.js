7fa74e130331f7582e515e0aa343c82241a91d0e
Joshua Hunter <joshuahunter@Andrews-MBP-2.lan>
2023-08-20 12:40:46 -0500

    <just another commit>

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -2,11 +2,17 @@
 
    // console.log("This does not print.");
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+   console.log("Hello, World!"); //Comments do not have to start at the beginning of a line.
+   //This is my single Line comment
+
 
    /* Here is how
    to have
    multi-line
    comments. */
-
+   /* This is
+   my multi-line
+   multi-dimensional 
+   comment */
+  
    console.log("Comments make your code more readable by others.");
